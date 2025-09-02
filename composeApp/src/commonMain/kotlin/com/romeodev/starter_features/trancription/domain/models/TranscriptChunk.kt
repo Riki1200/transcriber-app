107dea6c9b5da1fcde8d28a8d4d7cb95ad614419
Romeo <45272844+Riki1200@users.noreply.github.com>
2025-09-02 09:17:15 -0400

    Rename MainScreenViewModel for improved clarity and organization

--- composeApp/src/commonMain/kotlin/com/romeodev/starter_features/trancription/domain/models/TranscriptChunk.kt
+++
@@ -1,7 +0,0 @@
-package com.romeodev.starter_features.trancription.domain.models
-
-data class TranscriptChunk(
-    val text: String,
-    val startSec: Double? = null,
-    val endSec: Double? = null
-)
--- composeApp/src/commonMain/kotlin/com/romeodev/starter_features/trancription/domain/models/TranscriptConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.romeodev.starter_features.trancription.domain.models
-
-data class TranscriptConfig(
-    val languageHint: String? = null,
-    val enableTimestamps: Boolean = false
-)
